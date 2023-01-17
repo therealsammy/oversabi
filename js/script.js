bf2711b100cb034dc622baaacd213e04f669ba0f
Samuel Ohiri <therealsammy18x@gmail.com>
2023-01-17 13:05:21 +0100

    swiper

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 (function () {
   "use strict";
 
+  // Menu bar
+
   const menu = document.querySelector(".menu-icon");
 
   menu.addEventListener("click", () => {
     menu.classList.toggle("show");
+  });
+
+  // Reviews Swiper
+
+  const swiper = new Swiper(".reviews-content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
   });
 })();
