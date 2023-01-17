d6cf51d7683aba1384403fff6e9d5354192f0b0d
Samuel Ohiri <therealsammy18x@gmail.com>
2023-01-17 15:50:42 +0100

    email validation working

--- js/script.js
+++ js/script.js
@@ -16,15 +16,35 @@
     centeredSlides: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false,
+      disableOnInteraction: true,
     },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    // navigation: {
-    //   nextEl: ".swiper-button-next",
-    //   prevEl: ".swiper-button-prev",
-    // },
   });
+
+  // Email JS
+
+  function validate() {
+    const email = document.querySelector(".email");
+    const submit = document.querySelector(".subscribe-btn");
+    const emailFormat =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    submit.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      if (email.value.match(emailFormat)) {
+        // sendmail(email);
+        // success;
+        console.log("success");
+      } else {
+        // emptyerror();
+        console.log("error");
+      }
+    });
+  }
+
+  validate();
 })();
